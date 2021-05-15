bfe73bbb23575cfede2bda9f6dbb8049286b5405
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-15 22:42:35 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "namespace" {
   default     = "chaosmesh"
   description = "Namespace used for deploying the Chaos Mesh objects."
 }
+
+variable "create_namespace" {
+  type        = string
+  default     = true
+  description = "Determines if the namespace should be created by the module."
+}
