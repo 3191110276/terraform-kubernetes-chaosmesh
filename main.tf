3acb170a7f35b601533dd503e582bcc6c1eed89e
Michael Maurer <3191110276@users.noreply.github.com>
2021-05-15 22:42:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
+############################################################
+# REQUIRED PROVIDERS
+############################################################
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.2"
+    }
+  }
+}
 
+
+############################################################
+# CREATE APPDYNAMICS NAMESPACE
+############################################################
+resource "kubernetes_namespace" "chaosmesh" {
+  count = var.create_namespace ? 1 : 0
+
+  metadata {
+    name = var.namespace
+  }
+}
